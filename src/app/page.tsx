ee31d446c3749d8640b6ba4c2cfddade3fea9dcd
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-12-19 16:21:44 +0900

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      Authentication
+    <div className=' w-[1400px] h-[520px] m-5 border-2 border-orange-500 '>
+      <div>
+        <div>
+          <div>
+            Authentication
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
