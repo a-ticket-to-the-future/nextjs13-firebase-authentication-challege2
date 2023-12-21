e03f0cd1b7ba2bb37aceae828dfbae278847fd57
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-12-21 15:56:08 +0900

    コードを書く上での自分の悪い癖を理解しよう。コードの修正はできたよ。Popupからサインインはできました。

---
+++ src/app/App/SignOutButton/page.jsx
@@ -0,0 +1,35 @@
+// "use client"
+
+// import { auth } from '@/firebase'
+// import React, { useState } from 'react'
+// import AuthProvider, { useAuth } from '../../AuthProvider/page'
+// import {signOut as signOutFromFirebase} from "firebase/auth"
+
+// const SignOutButton = () => {
+//   const {authUser,setAuthUser} = useAuth();
+//   // const [signIn , setSignIn] = useState("");
+//   //   setSignIn(setAuthUser);
+    
+//   // console.log(authUser)
+  
+
+//   const signOut = async () => {
+//     try{
+//         await auth.signOut(auth);
+//         setAuthUser(undefined); 
+//         console.log("サインアウト成功");
+//     } catch(error) {
+//         console.log("サインアウトできていません",error)
+//     }
+//     }
+
+//   return (
+//     <div>
+//       <button className='border-2 border-red-600 mx-5 my-20 px-4 py-1 bg-red-700 text-slate-50 rounded-md font-bold hover:scale-110 active:scale-95' onClick={signOut}>
+//                     サインアウト
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default SignOutButton
